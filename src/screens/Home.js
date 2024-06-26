b80eeee3ec125ec8c2e97af5383d3f26b252670e
KymRhys2k22 <kymrhys@gmail.com>
2024-06-27 02:39:30 +0800

    second commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,35 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Home = () => {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingVertical: 100,
+        alignItems: "center",
+      }}>
+      <Text style={{ fontSize: 40, fontWeight: "bold", color: "gray" }}>
+        Price Check
+      </Text>
+      <MaterialIcons name="qr-code-scanner" size={300} color="black" />
+      <Text
+        style={{
+          fontSize: 40,
+          fontWeight: "bold",
+          marginBottom: 200,
+          color: "gray",
+        }}>
+        App
+      </Text>
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate("BarcodeItemScanner")}>
-        <Text style={styles.textButton}>Price Check</Text>
+        <Text style={styles.textButton}>Start Scanning</Text>
       </TouchableOpacity>
     </View>
   );
@@ -19,9 +39,10 @@ export default Home;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "blue",
+    backgroundColor: "#EF018D",
     paddingVertical: 20,
     width: 350,
+    borderRadius: 20,
   },
   textButton: {
     textAlign: "center",
